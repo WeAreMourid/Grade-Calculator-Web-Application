3fcf89d829f7689eb33c986e3d00e39373226fbf
WE_ARE_MOURID <152662262+Mourid-07@users.noreply.github.com>
2024-04-22 18:39:18 +0100

    Add files via upload
    Features:
    Allows users to input their control scores and end-of-module exam score.
    Calculates the final module grade using a predetermined formula.
    Provides visual feedback in the form of emojis indicating the user's performance level.
    Responsive design for usability across various devices.
    Technologies Used:
    HTML5
    CSS3
    JavaScript
    How to Use:
    Open the HTML file (index.html) in a web browser.
    Enter your control scores and end-of-module exam score into the respective input fields.
    Click the "Note Module" button to calculate your final module grade.
    View the calculated grade and the corresponding emoji indicating your performance level.

---
+++ project_1/face.js
@@ -0,0 +1,23 @@
+// function red(){
+//     let pop = document.getElementsByClassName("container")[0];
+//     pop.style.backgroundColor="red";
+// }
+
+// function green(){
+//     let pop = document.getElementsByClassName("container")[0];
+//     pop.style.backgroundColor="green";
+// }
+
+// function yellow(){
+//     let pop = document.getElementsByClassName("container")[0];
+//     pop.style.backgroundColor="yellow";
+// }
+
+// function blue(){
+//     let pop = document.getElementsByClassName("container")[0];
+//     pop.style.backgroundColor="blue";
+// }
+
+
+
+ellipse(200,)
